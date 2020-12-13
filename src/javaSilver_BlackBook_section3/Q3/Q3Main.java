245c5d3e089dffe8dd731690245e72291953d014
ma22npk <gunjo64@icloud.com>
2020-12-13 22:32:48 +0900

    Q3のサンプルコード

--- src/javaSilver_BlackBook_section3/Q3/Q3Main.java
+++ src/javaSilver_BlackBook_section3/Q3/Q3Main.java
@@ -3,8 +3,23 @@ package javaSilver_BlackBook_section3.Q3;
 public class Q3Main {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
+        // 明示的なキャスト
+        int a = 100;
+        // short型に変換
+        short b = (short) a;
 
+        // byte 型は -128 ~ 127までの値を扱うデータ型なので、範囲を超えるとエラーとなる。
+        // byte の範囲内のint型のリテラルのためcompile可能
+        byte c = 127;
+        // byte の範囲に収まらないint型のリテラルのためコンパイルエラー
+        byte d = 128;
+        // 10進数にすると256になるため、コンパイルエラー
+        byte e = 0b10000000;
+
+        int f = 2 * 3L;
+        float g = 10.0;
+
+        System.out.println(g);
     }
 
 }
