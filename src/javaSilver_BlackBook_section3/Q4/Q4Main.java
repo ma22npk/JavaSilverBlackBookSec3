0246508ead90031eef1dc98796cc6746cf6315c2
ma22npk <gunjo64@icloud.com>
2020-12-14 21:42:28 +0900

    黒本第3章Q5 四則演算

--- src/javaSilver_BlackBook_section3/Q4/Q4Main.java
+++ src/javaSilver_BlackBook_section3/Q4/Q4Main.java
@@ -5,7 +5,8 @@ public class Q4Main {
     public static void main(String[] args) {
         int a = 10;
         int b = a++ + a + a-- - a-- + ++a;
-        System.out.println(b);
+        //11 ,21, 30, 21 32
+        System.out.println(b); //32正解
 
     }
 
--- src/javaSilver_BlackBook_section3/Q5/Q5Main.java
+++ src/javaSilver_BlackBook_section3/Q5/Q5Main.java
@@ -4,6 +4,7 @@ public class Q5Main {
 
     public static void main(String[] args) {
         // TODO 自動生成されたメソッド・スタブ
+        System.out.println(100 % 20 * 30 + 100 / 20);
 
     }
 
