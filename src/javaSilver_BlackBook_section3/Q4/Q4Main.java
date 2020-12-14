7048af46c688216079d1843edf46da75e4416f5e
ma22npk <gunjo64@icloud.com>
2020-12-14 20:21:37 +0900

    黒本3章Q4 四則演算

--- src/javaSilver_BlackBook_section3/Q4/Q4Main.java
+++ src/javaSilver_BlackBook_section3/Q4/Q4Main.java
@@ -3,7 +3,9 @@ package javaSilver_BlackBook_section3.Q4;
 public class Q4Main {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
+        int a = 10;
+        int b = a++ + a + a-- - a-- + ++a;
+        System.out.println(b);
 
     }
 
